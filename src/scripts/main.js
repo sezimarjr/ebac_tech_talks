0c67c89ed86cf873d083151465024b2edf25c1fb
Sezimar Jr <sezimar_jrcs@hotmail.com>
2023-11-16 18:13:22 -0300

    finalizando projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
-console.log("olá mundo");
+AOS.init();
+
+const dataDoEvento = new Date("Nov 28, 2023 00:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+
+  const diasEmMs = 1000 * 60 * 60 * 24;
+  const horasEmMs = 1000 * 60 * 60;
+  const minutosEmMs = 1000 * 60;
+
+  const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+  const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+  const horasAteOEvento = Math.floor(
+    (distanciaAteOEvento % diasEmMs) / horasEmMs
+  );
+
+  const minutosAteOEvento = Math.floor(
+    (distanciaAteOEvento % horasEmMs) / minutosEmMs
+  );
+
+  const segundosAteOEvento = Math.floor(
+    (distanciaAteOEvento % minutosEmMs) / 1000
+  );
+
+  document.getElementById(
+    "contador"
+  ).innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+  if (distanciaAteOEvento < 0) {
+    clearInterval(contaAsHoras);
+    document.getElementById("contador").innerHTML = "Evento Expirado";
+  }
+}, 1000);
